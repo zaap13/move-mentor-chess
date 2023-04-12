26b6c4404be95f01b29ee4a608ab394e90140104
Felipe Bueno <felipebueno2201@gmail.com>
2023-04-11 21:04:49 -0300

    updated input

--- src/pages/NewCourse/NewCourse.js
+++ src/pages/NewCourse/NewCourse.js
@@ -2,40 +2,119 @@ import { useState } from "react";
 import { Chessboard } from "react-chessboard";
 import { Chess } from "chess.js";
 import styled from "styled-components";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import {
+  FaChessPawn,
+  FaChessKnight,
+  FaChessBishop,
+  FaChessRook,
+  FaChessQueen,
+} from "react-icons/fa";
 
 function FenInputChessboard() {
-  const [fen, setFen] = useState("start");
-  const [currentPosition, setCurrentPosition] = useState({});
+  const [fen, setFen] = useState(
+    "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+  );
+  const [position, setPosition] = useState({});
+  const [selectedPiece, setSelectedPiece] = useState(null);
+  const [inputValue, setInputValue] = useState(fen);
+  const [game, setGame] = useState(new Chess(fen));
+
+  const pieceButtons = [
+    { label: "Peão Branco", value: "wP", icon: FaChessPawn },
+    { label: "Cavalo Branco", value: "wN", icon: FaChessKnight },
+    { label: "Bispo Branco", value: "wB", icon: FaChessBishop },
+    { label: "Torre Branca", value: "wR", icon: FaChessRook },
+    { label: "Rainha Branca", value: "wQ", icon: FaChessQueen },
+    { label: "Peão Preto", value: "bP", icon: FaChessPawn },
+    { label: "Cavalo Preto", value: "bN", icon: FaChessKnight },
+    { label: "Bispo Preto", value: "bB", icon: FaChessBishop },
+    { label: "Torre Preta", value: "bR", icon: FaChessRook },
+    { label: "Rainha Preta", value: "bQ", icon: FaChessQueen },
+  ];
 
   function handleFenChange(e) {
-    setFen(e.target.value);
+    if (e.keyCode === 13) {
+      const newFen = e.target.value;
+
+      try {
+        setFen(newFen);
+        setInputValue(newFen);
+      } catch (error) {
+        toast.warning(error.message);
+        setFen(fen);
+        setInputValue(fen);
+      }
+    }
   }
 
   function onChange(currentPosition) {
-    const fen = objToFen(currentPosition, "w", "KQkq", "-", 0);
-    console.log(currentPosition);
-    const chess = new Chess(fen);
-    const newFen = chess.fen();
+    const newFen = objToFen(currentPosition, "w", "KQkq", "-", 0);
+
+    try {
+      new Chess(newFen);
+    } catch (error) {
+      toast.warning(error.message);
+      return;
+    }
     setFen(newFen);
-    console.log(newFen);
-    setCurrentPosition(currentPosition);
+
+    setInputValue(newFen);
+    setPosition(currentPosition);
   }
 
   function onSquareClick(square) {
     // Aqui você pode adicionar a lógica para adicionar uma peça no quadrado clicado
-    const newCurrentPosition = {
-      ...currentPosition,
-      [square]: "wN", // adiciona um cavalo branco na posição clicada
-    };
-    onChange(newCurrentPosition);
-    setCurrentPosition(newCurrentPosition);
+    try {
+      if (!position[square] && selectedPiece) {
+        const newposition = {
+          ...position,
+          [square]: selectedPiece, // adiciona um cavalo branco na posição clicada
+        };
+        const fen = objToFen(newposition, "w", "KQkq", "-", 0);
+        setGame(new Chess(fen));
+      }
+    } catch (error) {
+      toast.warning(error.message);
+    }
   }
 
   function onSquareRightClick(square) {
-    const newCurrentPosition = { ...currentPosition };
-    delete newCurrentPosition[square];
-    onChange(newCurrentPosition);
-    setCurrentPosition(newCurrentPosition);
+    try {
+      const newposition = { ...position };
+      delete newposition[square];
+      const fen = objToFen(newposition, "w", "KQkq", "-", 0);
+      setGame(new Chess(fen));
+    } catch (error) {
+      toast.warning(error.message);
+    }
+  }
+
+  function handlePieceSelect(value) {
+    setSelectedPiece(value);
+  }
+
+  function onPieceDrop(sourceSquare, targetSquare, piece) {
+    if (position[targetSquare]) {
+      if (position[targetSquare][1] === "K") {
+        return false;
+      }
+    }
+    try {
+      const newposition = { ...position, [targetSquare]: piece };
+      delete newposition[sourceSquare];
+      const toFen = objToFen(newposition, "w", "KQkq", "-", 0);
+      setFen(toFen);
+      setInputValue(toFen);
+      setPosition(newposition);
+      setGame(new Chess(toFen));
+      return true;
+    } catch (error) {
+      toast.warning(error.message);
+
+      return false;
+    }
   }
 
   function objToFen(obj, player, castling, enPassant, halfMoves) {
@@ -89,16 +168,40 @@ function FenInputChessboard() {
   }
 
   return (
-    <Board>
-      <Chessboard
-        boardWidth={500}
-        position={fen}
-        getPositionObject={onChange}
-        onSquareClick={onSquareClick}
-        onSquareRightClick={onSquareRightClick}
-      />
-      <input type="text" value={fen} onChange={handleFenChange} />
-    </Board>
+    <>
+      <Board>
+        <h1>{selectedPiece}</h1>
+        {pieceButtons.map((button) => (
+          <button
+            key={button.value}
+            onClick={() => handlePieceSelect(button.value)}
+            style={{
+              backgroundColor:
+                selectedPiece === button.value ? "lightblue" : "white",
+            }}
+          >
+            {button.label}
+          </button>
+        ))}
+        {fen}
+
+        <Chessboard
+          boardWidth={500}
+          position={game.fen()}
+          onPieceDrop={onPieceDrop}
+          getPositionObject={onChange}
+          onSquareClick={onSquareClick}
+          onSquareRightClick={onSquareRightClick}
+        />
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          onKeyDown={handleFenChange}
+        />
+      </Board>
+      <ToastContainer theme="dark" />
+    </>
   );
 }
 
